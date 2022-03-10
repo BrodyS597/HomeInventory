b2fe7a7df0dda4cebf74c89f46a8b0a0ac16ee68
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-03-10 12:53:32 -0700

    Created the HomeView storyboard and associated Cell storyboards, as well as placeholder controller files

---
+++ HomeInventory/HomeInventory/HomeView/HomeAddCellCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeAddCellCollectionViewCell.swift
+//  HomeInventory
+//
+//  Created by Brody Sears on 3/10/22.
+//
+
+import UIKit
+
+class HomeAddCellCollectionViewCell: UICollectionViewCell {
+    
+}
