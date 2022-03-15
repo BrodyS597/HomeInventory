abffbc880bfd311e62cca047455bf4497c6cb79a
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-03-15 14:46:34 -0600

    abstracted model structure

--- HomeInventory/HomeInventory/Models/Collection.swift
+++ HomeInventory/HomeInventory/Models/Collection.swift
@@ -19,29 +19,4 @@ class Collection {
     }
 }
 
-class Item {
-    
-    // MARK: -Properties
-    var itemName: String
-    var itemPhotoURL: URL?
-    var model: String
-    var serialNumber: String
-    var purchasePrice: Double
-    var valuePrice: Double
-    var purchaseDate: Date
-    var itemCategory: String
-    var notes: String
 
-    // MARK: -INIT
-    init(itemName: String, itemPhotoURL: URL? = nil, model: String, serialNumber: String, purchasePrice: Double, valuePrice: Double, purchaseDate: Date = Date (), itemCategory: String, notes: String) {
-        self.itemName = itemName
-        self.itemPhotoURL = itemPhotoURL
-        self.model = model
-        self.serialNumber = serialNumber
-        self.purchasePrice = purchasePrice
-        self.valuePrice = valuePrice
-        self.purchaseDate = purchaseDate
-        self.itemCategory = itemCategory
-        self.notes = notes
-    }
-}
