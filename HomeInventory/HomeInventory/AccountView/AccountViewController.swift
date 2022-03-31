30dc6efc16e3b2e6121b2c5f7a8b6b5151924141
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-03-31 17:19:41 -0600

    Created contact us and logout button with method signatures, and implemented the privacy policy

--- HomeInventory/HomeInventory/AccountView/AccountViewController.swift
+++ HomeInventory/HomeInventory/AccountView/AccountViewController.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class AccountViewController: UIViewController {
 
+    @IBOutlet weak var userEmailAddressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
         // Do any additional setup after loading the view.
     }
     
-
-    //holds user object
-
+    func updateUI() {
+        if let user = Auth.auth().currentUser {
+            if let emailAddress = user.email {
+            self.userEmailAddressLabel.text = "Signed in with: \(emailAddress)"
+            }
+        }
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func contactUsButtonTapped(_ sender: Any) {
+    }
+    
+    
 }
--- HomeInventory/HomeInventory/Resources/AppDelegate.swift
+++ HomeInventory/HomeInventory/Resources/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-//        if let user = Auth.auth().currentUser {
-//            print("Signed in as \(user.uid), email: \(user.email)")
-//        }
+        if let user = Auth.auth().currentUser {
+            print("Signed in as \(user.uid), email: \(user.email)")
+        }
         // Override point for customization after application launch.
         return true
     }
