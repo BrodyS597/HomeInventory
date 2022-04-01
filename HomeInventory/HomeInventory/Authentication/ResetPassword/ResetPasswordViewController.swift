b54cb778a7bd5b7aad5e26549521cf1653f063e5
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-04-01 12:47:18 -0600

    Created password reset and its associated alert controller, functionality

--- HomeInventory/HomeInventory/Authentication/ResetPassword/ResetPasswordViewController.swift
+++ HomeInventory/HomeInventory/Authentication/ResetPassword/ResetPasswordViewController.swift
@@ -21,16 +21,22 @@ class ResetPasswordViewController: UIViewController {
     
     // MARK: -IBActions
     @IBAction func sendPasswordRestButtonTapped(_ sender: Any) {
+        sendPasswordReset()
+        
+        let alertController = UIAlertController(title: "If an account is registered to that email address, we will send you an email", message: "Please check your email for further instructions on resetting your password", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Confirm and return to login", style: .default , handler: { (action: UIAlertAction!) in
+            //confirm logic
+            self.returnToLogin()
+        }))
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func returnToLoginButtonTapped(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "LoginView", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "login")
-        viewController.modalPresentationStyle = .overFullScreen
-        present(viewController, animated: true)
+        returnToLogin()
     }
     
-    func sendPasswordReset(){
+    func sendPasswordReset() {
         guard let emailAddress = emailAddressTextField.text else { return }
         Auth.auth().sendPasswordReset(withEmail: emailAddress) { error in
             if let error = error {
@@ -40,8 +46,11 @@ class ResetPasswordViewController: UIViewController {
         }
     }
     
-    func checkUserInfo(){
-        
+    func returnToLogin() {
+        let storyboard = UIStoryboard(name: "LoginView", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "login")
+        viewController.modalPresentationStyle = .overFullScreen
+        present(viewController, animated: true)
     }
     
 }
