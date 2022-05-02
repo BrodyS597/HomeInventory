b5b1d3e6eb438ab8837a1af07ff9c289a10901f7
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-05-01 18:13:51 -0600

    Added function to delete a collection

--- HomeInventory/HomeInventory/HomeView/HomeCollectionViewCell.swift
+++ HomeInventory/HomeInventory/HomeView/HomeCollectionViewCell.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class HomeCollectionViewCell: UICollectionViewCell {
+protocol HomeCollectionViewCellDelegate {
+    func presentAlertToDelete(collection: Collection)
+}
+
+class HomeCollectionViewCell: UICollectionViewCell, UIGestureRecognizerDelegate {
     
     // MARK: -IBOutlets
     @IBOutlet weak var groupImageView: UIImageView!
     @IBOutlet weak var cellLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
+    var collection: Collection?
+    var delegate: HomeCollectionViewCellDelegate?
     
     override func prepareForReuse() {
         groupImageView.image = nil
@@ -25,13 +28,10 @@ class HomeCollectionViewCell: UICollectionViewCell {
     public func configure(with collection: Collection) {
         fetchImages(collection: collection)
         cellLabel.text = collection.name
+        self.collection = collection
     }
     
-    static func nib() -> UINib {
-        return UINib(nibName: "", bundle: nil)
-    }
-    
-    func fetchImages(collection: Collection) {        
+    func fetchImages(collection: Collection) {
         FireBaseStorageController().loadImageFromCollection(fromCollection: collection) { result in
             switch result {
             case .success(let image):
@@ -42,6 +42,20 @@ class HomeCollectionViewCell: UICollectionViewCell {
                 self.groupImageView.image = nil
             }
         }
+        setupLongGestureRecognizerOnCell()
+    }
+    
+    private func setupLongGestureRecognizerOnCell() {
+        let longPressedGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        longPressedGesture.minimumPressDuration = 0.5
+        longPressedGesture.delegate = self
+        longPressedGesture.delaysTouchesBegan = true
+        self.contentView.addGestureRecognizer(longPressedGesture)
+    }
+    
+    @objc func handleLongPress() {
+        guard let collection = collection else { return }
+        delegate?.presentAlertToDelete(collection: collection)
     }
     
 }
