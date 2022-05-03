9872ec8d74ae9302111fc1c6c84f5bab267a8ac5
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-05-03 13:33:48 -0600

    CSV export is now functional

--- HomeInventory/HomeInventory/ReportView/ReportViewController.swift
+++ HomeInventory/HomeInventory/ReportView/ReportViewController.swift
@@ -24,6 +24,36 @@ class ReportViewController: UIViewController {
         viewModel = ReportViewModel(delegate: self)
     }
     
+    @IBAction func PDFButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func CSVButtonTapped(_ sender: Any) {
+        print("Exporting as CSV")
+        //CSV File Name
+        let file_name = "My Home Inventory.csv"
+        // This is where the file will temporarily be saved to the device
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(file_name)
+        //Column titles
+        var csvHead = "Collection, Item, Model, Serial #, Purchase Price, Value Price, Purchase Date, Category, Notes\n"
+        guard let collections = viewModel.collections else { return }
+        for collection in collections {
+            //csvHead.append("\(collection.name)")
+            for item in collection.items {
+                csvHead.append("\(collection.name),\(item.itemName),\(item.model),\(item.serialNumber),\(item.purchasePrice),\(item.valuePrice),\(item.purchaseDate),\(item.itemCategory),\(item.notes)\n")
+            }
+        }
+        do {
+            try csvHead.write(to: path!, atomically: true, encoding: .utf8)
+            let exportSheet = UIActivityViewController(activityItems: [path as Any], applicationActivities: nil)
+            self.present(exportSheet, animated: true, completion: nil)
+            print("Exported")
+        } catch {
+            print("Error exporting")
+        }
+    }
+    
+    
     private func updateUI() {
        self.totalValueLabel.text = "\(viewModel.calculateNumberOfItems()) items totaling $\(viewModel.calculateTotalValue())"
         
